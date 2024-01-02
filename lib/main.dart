ff1995403d0432ac21e8e2fee2f835a9a20c39cc
Belal Eid <91228516+belaleid5@users.noreply.github.com>
2024-01-02 20:59:33 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shoop_smart_admin/firebase_options.dart';
+import 'consts/theme_data.dart';
+import 'providers/products_provider.dart';
+import 'providers/theme_provider.dart';
+import 'screens/dashboard_screen.dart';
+import 'screens/edit_upload_product_form.dart';
+import 'screens/inner_screen/orders/orders_screen.dart';
+import 'screens/search_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<FirebaseApp>(
+        future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        ),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const MaterialApp(
+              debugShowCheckedModeBanner: false,
+              home: Scaffold(
+                body: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              ),
+            );
+          } else if (snapshot.hasError) {
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              home: Scaffold(
+                body: Center(
+                  child: SelectableText(snapshot.error.toString()),
+                ),
+              ),
+            );
+          }
+          return MultiProvider(
+            providers: [
+              ChangeNotifierProvider(create: (_) {
+                return ThemeProvider();
+              }),
+              ChangeNotifierProvider(create: (_) {
+                return ProductsProvider();
+              }),
+            ],
+            child: Consumer<ThemeProvider>(
+                builder: (context, themeProvider, child) {
+              return MaterialApp(
+                debugShowCheckedModeBanner: false,
+                title: 'Shop Smart ADMIN EN',
+                theme: Styles.themeData(
+                    isDarkTheme: themeProvider.getIsDarkTheme,
+                    context: context),
+                home: const DashboardScreen(),
+                routes: {
+                  OrdersScreenFree.routeName: (context) =>
+                      const OrdersScreenFree(),
+                  SearchScreen.routeName: (context) => const SearchScreen(),
+                  EditOrUploadProductScreen.routeName: (context) =>
+                      const EditOrUploadProductScreen(),
+                },
+              );
+            }),
+          );
+        });
+  }
+
+
+}
